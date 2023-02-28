0aed6fe11a5780ee1cc07ac46a1862fcbb27182f
Dhruv Patel <dhruv225@outlook.com>
2023-02-28 17:02:39 -0500

    created popup for user input

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const button = document.querySelector('.create');
+const tododiv = document.querySelector('.todos');
+const popup = document.querySelector('.popup');
+const postTodo = document.querySelector('.postTodo');
+class todos{
 
+
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    } 
+}
+
+
+
+
+function popUp(){
+   popup.style.display = "block";
+   console.log('ff');
+}
+
+button.addEventListener('click', popUp)
+
+postTodo.addEventListener('click', () =>{
+    popup.style.display = "none";
+})
